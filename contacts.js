d42b54c569d2d3d32584d9462e29471e27048dd8
Kovalchuk63 <vov4uk4763@gmail.com>
2023-12-11 10:24:42 +0200

    v_3

---
+++ contacts.js
@@ -0,0 +1,69 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const contactsService = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+  const buffer = await fs.readFile(contactsPath);
+  const data = JSON.parse(buffer);
+  return data;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contactId === contact.id) || null;
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+
+  const contactToDelete = contacts.findIndex(
+    (contact) => contactId === contact.id
+  );
+  if (contactToDelete !== -1) {
+    const deletedContact = contacts.splice(contactToDelete, 1);
+    await contactsService(contacts);
+    return deletedContact;
+  } else {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+  const contacts = await listContacts();
+  if (
+    contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    )
+  ) {
+    return { message: "Contact exists" };
+  } else {
+    const contact = {
+      name,
+      email,
+      phone,
+      id: nanoid(),
+    };
+
+    contacts.push(contact);
+    await contactsService(contacts);
+    return contact;
+  }
+}
+
+const contacts = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+module.exports = contacts;
